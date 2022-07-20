d8215ec4b8b3113419438d304c83856489d47f73
Nick Place <nplace.np@gmail.com>
2022-07-19 20:27:31 -0700

    Adding input system setup to script + player ship

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,22 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+
+    [SerializeField] InputAction movement;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
 
-        float verticalThrow = Input.GetAxis("Vertical");
+
+        //float horizontalThrow = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalThrow);
+        //
+        //float verticalThrow = Input.GetAxis("Vertical");
         Debug.Log(verticalThrow);
     }
 }
